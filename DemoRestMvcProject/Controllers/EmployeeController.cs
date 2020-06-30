243e9e42a2597e56e65c72c69c4f667b3a86542c
ivansimeonov <ivannicksimeonov@gmail.com>
2020-06-30 14:05:12 -0700

    Routing testing

--- DemoRestMvcProject/Controllers/EmployeeController.cs
+++ DemoRestMvcProject/Controllers/EmployeeController.cs
@@ -7,13 +7,15 @@ using System.Web.Http;
 using DemoRestMvcProject.Domain.Models;
 using DemoRestMvcProject.Domain.Repositories;
 using DemoRestMvcProject.Services;
+using Swashbuckle.Swagger.Annotations;
 
 namespace DemoRestMvcProject.Controllers
 {
     public class EmployeeController : ApiController
     {
-       
+
         // GET: api/Employee
+        [SwaggerOperation("Get all Employees")]
         public IEnumerable<Employee> Get()
         {
             EmployeeRepository repo = new EmployeeRepository();
@@ -22,11 +24,20 @@ namespace DemoRestMvcProject.Controllers
         }
 
         // GET: api/Employee/5
+        [SwaggerOperation("Get Employee by id")]
+
         public Employee Get(int id)
         {
             EmployeeRepository repo = new EmployeeRepository();
             EmployeeService service = new EmployeeService(repo);
             return service.GetEmployeeById(id);
+        }
+
+        [SwaggerOperation("Test")]
+        [Route("test")]
+        public String GetTestNullReturn()
+        {
+            return "test";
         }
     }
 }
